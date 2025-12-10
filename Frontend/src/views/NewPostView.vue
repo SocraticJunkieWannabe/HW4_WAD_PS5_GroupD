7c3e62b3de37ad006873ff2d6d9376b0bc8985f7
g le-chartier <g.le-chartier@ecam.fr>
2025-12-10 17:13:22 +0200

    Setup newpost component and view

--- Frontend/src/views/NewPostView.vue
+++ Frontend/src/views/NewPostView.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="new_post">
-    <h1>This is a new post page</h1>
-  </div>
+
 </template>
+
+<script>
+// @ is an alias to /src
+import PostsCompo from "@/components/NewPost.vue"
+
+export default {
+  name: 'NewPostView',
+  components: {
+    NewPost
+  },
+
+  methods: {
+    DeleteAll: function() {
+        fetch('http://localhost:3000/posts/deleteAll', {
+          method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(data => {
+          this.$refs.childComp.postList();
+        })  
+    },
+    AddPost: function() {
+        this.$router.push('/post')
+    },
+    Logout: function(){
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+button {
+  margin-top: 75px;
+  border-radius: 30px;
+  width: fit-content;
+  padding: 10px 20px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.post-actions 
+{
+  display: flex;
+  justify-content: space-between;
+}
+</style>
